b9057884a0d21d57620bf1787e5231f2b8cbd3c6
hhharrisonnn <harrison.ho19@gmail.com>
2021-02-17 20:23:20 +0000

    Third commit

--- commands/test.js
+++ commands/test.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "this is a test command",
   execute(message, args) {
     
-    if (message.member.roles.cache.some(r => r.name === 'mod')) {
+    if (message.member.roles.cache.some(r => r.name === 'K4')) {
       message.channel.send('Testing works');
     } else {
       message.channel.send('You don\'t have the permissions to use this command.');
