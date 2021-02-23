ad1aa050e1da4396054c105c788e3d566e27eaf6
hhharrisonnn <harrison.ho19@gmail.com>
2021-02-23 18:37:49 +0000

    go agane

--- events/guild/guildMemberAdd.js
+++ events/guild/guildMemberAdd.js
@@ -1,12 +1,11 @@
-const { TeamMember } = require('discord.js');
 const profileModel = require('../../models/profileSchema');
 
-module.exports = async(message, args, cmd, client, Discord) => {
+module.exports = async(client, Discord) => {
   let profile = await profileModel.create({
     userID: member.id,
     serverID: member.guild.id,
     coins: 1000,
     bank: 0
-  })
+  });
   profile.save();
 }
