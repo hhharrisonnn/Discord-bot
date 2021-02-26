5c54ca94d69a3d4cf5c6a2c25c51a204fd91a06d
hhharrisonnn <harrison.ho19@gmail.com>
2021-02-26 11:29:38 +0000

    reverted

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,10 +1,3 @@
 module.exports = () => {
   console.log('K4 Bot is online.');
-  client.user.setPresence ({
-    status: "online",
-    game: {
-      name: "$c",
-      type: "PLAYING"
-    }
-  });
 }
