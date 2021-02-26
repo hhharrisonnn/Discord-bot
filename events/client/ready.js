1c6af0799e6db550851380440deacd21e96900cb
hhharrisonnn <harrison.ho19@gmail.com>
2021-02-26 11:10:11 +0000

    set presence

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,3 +1,10 @@
 module.exports = () => {
   console.log('K4 Bot is online.');
+  client.user.setPresence ({
+    status: "online",
+    game: {
+      name: "$c",
+      type: "PLAYING"
+    }
+  });
 }
