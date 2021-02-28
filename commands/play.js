27527fab1c2ec4d36506106e9c60a9eaf877f575
hhharrisonnn <harrison.ho19@gmail.com>
2021-02-28 09:41:59 +0000

    added embed for play

--- commands/play.js
+++ commands/play.js
@@ -94,8 +94,8 @@ const video_player = async (guild, song) => {
     song_queue.songs.shift();
     video_player(guild, song_queue.songs[0]);
   });
-  var newEmbed2 = new Discord.MessageEmbed()
   await song_queue.text_channel.send(newEmbed2)
+  var newEmbed2 = new Discord.MessageEmbed()
   .setColor('#304281')
   .setTitle('Music :notes:')
   .addFields(
