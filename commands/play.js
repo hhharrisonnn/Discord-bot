a4fed5f26c471b12cf15461aa7663ad64cde2c1d
hhharrisonnn <harrison.ho19@gmail.com>
2021-02-28 09:46:46 +0000

    added embed for play

--- commands/play.js
+++ commands/play.js
@@ -94,14 +94,14 @@ const video_player = async (guild, song) => {
     song_queue.songs.shift();
     video_player(guild, song_queue.songs[0]);
   });
-  await song_queue.text_channel.send(newEmbed2)
-  var newEmbed2 = new Discord.MessageEmbed()
+  const newEmbed2 = new Discord.MessageEmbed()
   .setColor('#304281')
   .setTitle('Music :notes:')
   .addFields(
     {name: 'Playing:', value: `:play_pause: Now playing **${song.title}** 🎶`},
   )
   .setFooter('Bot made by Harrison for the K4 boys :)')
+  await song_queue.text_channel.send(newEmbed2)
   //await song_queue.text_channel.send(`:play_pause: Now playing **${song.title}** 🎶`);
 }
 
