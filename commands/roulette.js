6d7fa50ca84328838020426963fc007d025698b6
hhharrisonnn <harrison.ho19@gmail.com>
2021-03-06 19:38:11 +0000

    test

--- commands/roulette.js
+++ commands/roulette.js
@@ -1,30 +1,25 @@
-const profileModel = require("../models/profileSchema");
-module.exports = {
-  name: 'roulette',
-  aliases: ['r'],
-  description: 'roulette coins',
-  async execute(message, args, cmd, client, Discord, profileData) {
-    const amount = args[0];
-    if(amount % 1 != 0 || amount <= 0) return message.channel.send('Please enter a valid amount of coins to roulette.');
-    try {
-      if(amount > profileData.coins) return message.channel.send(`You don't have that amount of coins to roulette.`);
-      const randomNumber = Math.floor(Math.random() * 2);
-      if (randomNumber = 1) {
-        var win = args[0] * 2 ;
-        return message.channel.send(`You won ${amount} coins!`);
-      } else {
-        var win = -args[0];
-      }
-      await profileModel.findOneAndUpdate({
-        userID: message.author.id
-      }, {
-        $inc: {
-          coins: win,
-        },
-      });
+// const profileModel = require("../models/profileSchema");
+// module.exports = {
+//   name: 'deposit',
+//   aliases: ['dep'],
+//   description: 'deposit coins',
+//   async execute(message, args, cmd, client, Discord, profileData) {
+//     const amount = args[0];
+//     if(amount % 1 != 0 || amount <= 0) return message.channel.send('Please enter a valid amount of coins to deposit.');
+//     try {
+//       if(amount > profileData.coins) return message.channel.send(`You don't have that amount of coins to deposit.`);
+//       await profileModel.findOneAndUpdate({
+//         userID: message.author.id
+//       }, {
+//         $inc: {
+//           coins: -amount,
+//           bank: amount,
+//         },
+//       });
 
-    }catch(err) {
-      console.log(err)
-    }
-  },
-}
+//       return message.channel.send(`You deposited ${amount} coins into your bank.`)
+//     }catch(err) {
+//       console.log(err)
+//     }
+//   },
+// }
