77653ac717ee42fd14088ccc3e59b81ba5687d66
hhharrisonnn <harrison.ho19@gmail.com>
2021-03-06 21:25:57 +0000

    added roulette, hope this works pausechamp

---
+++ commands/roulette.js
@@ -0,0 +1,31 @@
+const profileModel = require("../models/profileSchema");
+module.exports = {
+  name: 'roulette',
+  aliases: ['r'],
+  permissions: [],
+  description: 'roulette coins',
+  async execute(message, args, cmd, client, Discord, profileData) {
+    const amount = args[0];
+    if(amount % 1 != 0 || amount <= 0) return message.channel.send('Please enter a valid amount of coins to roulette.');
+    try {
+      if(amount > profileData.coins) return message.channel.send(`You don't have that amount of coins to roulette.`);
+      const randomNumber = Math.floor(Math.random() * 2);
+      if (randomNumber = 1) {
+        var win = args[0] * 2 
+        return message.channel.send(`You won ${amount} coins!`);
+      } else {
+        var win = -args[0] 
+      }
+      await profileModel.findOneAndUpdate({
+        userID: message.author.id
+      }, {
+        $inc: {
+          coins: win,
+        },
+      });
+
+    }catch(err) {
+      console.log(err)
+    }
+  },
+} 
