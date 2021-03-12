74fdd974abb3b32c0a5e3482f5bbda3ea5e75a16
hhharrisonnn <harrison.ho19@gmail.com>
2021-03-12 19:23:13 +0000

    dababy

---
+++ commands/dababy.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'dababy',
+  permissions: [],
+  cooldown: 0,
+  description: "sends pic of dababy",
+  execute(message, args) {
+    message.channel.send('https://i.imgur.com/PWoTYCq.jpg');
+  }
+}
--- commands/rob.js
+++ commands/rob.js
@@ -1,32 +1,35 @@
-const profileModel = require("../models/profileSchema");
-module.exports = {
-  name: 'rob',
-  aliases: ['r'],
-  permissions: [],
-  cooldown: 86400,
-  description: 'steal moneyz from people',
-  async execute(message, args, cmd, client, Discord, profileData) {
-    if(!args.length) return message.channel.send('You need to mention the player to rob them dummy.');
-    let target = message.mentions.users.first();
-    if(!target) return message.channel.send('That person does not exist.');
+// const profileModel = require("../models/profileSchema");
+// module.exports = {
+//   name: 'rob',
+//   aliases: ['r'],
+//   permissions: [],
+//   cooldown: 86400,
+//   description: 'steal moneyz from people',
+//   async execute(message, args, cmd, client, Discord, profileData) {
+//     if(!args.length) return message.channel.send('You need to mention the player to rob them dummy.');
+//     let target = message.mentions.users.first();
+//     if(!target) return message.channel.send('That person does not exist.');
 
-    const randomNumber = Math.random().toFixed(2);
+//     const randomNumber = Math.random().toFixed(2);
 
-    try {
-      const targetData = await profileData.findOne({ userID: target.id });
-      if(!targetData) return message.channel.send('This person does not exist.')
-      let robbed = profileData.coins * randomNumber
-      await profileModel.findOneAndUpdate({
-        userID: target.id
-      }, {
-        $inc: {
-          coins: robbed,
-        },
-      });
+//     function targetCoins() {
+      
+//     }
 
-      return message.channel.send(``);
-    } catch(err) {
-      console.log(err);
-    }
-  }
-}
+//     try {
+//       const targetData = await profileData.findOne({ userID: target.id });
+//       if(!targetData) return message.channel.send('This person does not exist.')
+//       await profileModel.findOneAndUpdate({
+//         userID: target.id
+//       }, {
+//         $inc: {
+//           coins: ,
+//         },
+//       });
+
+//       return message.channel.send(``);
+//     } catch(err) {
+//       console.log(err);
+//     }
+//   }
+// }
